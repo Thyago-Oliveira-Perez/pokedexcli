e676911364abb659d32bff101a5eb802d95bf600
Thyago de Oliveira Perez <thyagodoliveiraperez@gmail.com>
2026-01-17 11:18:22 -0300

    feat: turn into REPL and add function maps

--- main.go
+++ main.go
@@ -1,7 +1,43 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+type cliCommand struct {
+	name        	string
+	description 	string
+	callback     	func() error
+}
 
 func main() {
-	fmt.Println("Hello, World!")
+	scanner := bufio.NewScanner(os.Stdin)
+	commands := getCommands()
+	
+	for {
+		fmt.Print("Pokedex > ")
+		if !scanner.Scan() {
+			break
+		}
+		input := scanner.Text()
+		words := cleanInput(input)
+		
+		if len(words) == 0 {
+			continue
+		}
+		
+		commandName := words[0]
+		if cmd, exists := commands[commandName]; exists {
+			if err := cmd.callback(); err != nil {
+				fmt.Println("Error:", err)
+			}
+		} else {
+			fmt.Println("Unknown command:", commandName)
+		}
+	}
+	if err := scanner.Err(); err != nil {
+		fmt.Fprintln(os.Stderr, "Error reading input:", err)
+	}
 }
--- repl.go
+++ repl.go
@@ -1,6 +1,10 @@
 package main
 
-import "strings"
+import (
+	"fmt"
+	"os"
+	"strings"
+)
 
 func cleanInput(text string) []string {
 	text = strings.TrimSpace(text)
@@ -10,4 +14,30 @@ func cleanInput(text string) []string {
 		return []string{}
 	}
 	return words
+}
+
+func getCommands() map[string]cliCommand {
+	return map[string]cliCommand{
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    exitCallbackFunction,
+		},
+		"help": {
+			name:        "help",
+			description: "Displays a help message",
+			callback:    helpCallbackFunction,
+		},
+	}
+}
+
+func exitCallbackFunction() error {
+	fmt.Println("Closing the Pokedex... Goodbye!")
+	os.Exit(0)
+	return nil
+}
+
+func helpCallbackFunction() error {
+	fmt.Println("\nWelcome to the Pokedex!\nUsage:\n\nhelp: Displays a help message\nexit: Exit the Pokedex")
+	return nil
 }
